36570b674bd6dc8e97d390ac0f5968b7eadd9116
Ruben De Vries <rubendevries@Rubens-MacBook-Pro.local>
2025-10-24 19:57:11 +0200

    fix(tailwind): widen content paths and purge stale cache

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './**/*.{js,ts,jsx,tsx,mdx}', // 👈 add this catch-all line
   ],
   theme: { extend: {} },
   plugins: [],
